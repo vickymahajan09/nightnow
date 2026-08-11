4f0950bf2e136168bb3ec23f885c44452e3df6c0
vickymahajan09 <mahajanvicky04@gmail.com>
2026-08-11 21:30:41 +0530

    Update product page

--- app/admin/product/[id]/page.tsx
+++ app/admin/product/[id]/page.tsx
@@ -52,7 +52,7 @@ export default function AddProductPage() {
   // =========================================
   // IMAGES
   // =========================================
-
+//test
   const [images, setImages] =
     useState<string[]>([]);
 
